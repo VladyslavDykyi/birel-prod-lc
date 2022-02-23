7170fa1b3db992f1e6e8f230d1eb694794ea5f99
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2022-02-23 17:59:10 +0200

    upd

--- dist/js/pages/page-lc-company.js
+++ dist/js/pages/page-lc-company.js
@@ -4,5 +4,10 @@ import {bindTabs,} from '../default/module-lc.js';
 bindTabs('#tabs');
 
 $(document).ready(function() {
-	$('.js-example-basic-single').select2();
+	$('.js-example-basic-single').select2({
+		theme: "my",
+	});
+	$('.js-example-basic-single2').select2({
+		theme: "my-one-line",
+	});
 });
