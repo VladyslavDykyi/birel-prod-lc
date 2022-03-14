cc9c05727eb1419b1623f4ec30247c1387f4c3ee
Vladyslav Dykyi <vladislavdykyi33@gmail.com>
2022-03-14 23:24:50 +0200

    upd

--- dist/js/default/default-lc.js
+++ dist/js/default/default-lc.js
@@ -25,3 +25,12 @@ document.addEventListener('click', e => {
 		toggleMenu();
 	}
 });
+
+$(document).ready(function() {
+	$('.js-example-basic-single').select2({
+		theme: "my",
+	});
+	$('.js-example-basic-single2').select2({
+		theme: "my-one-line",
+	});
+});
--- dist/js/pages/page-lc-company.js
+++ dist/js/pages/page-lc-company.js
@@ -3,11 +3,3 @@ import {bindTabs,} from '../default/module-lc.js';
 
 bindTabs('#tabs');
 
-$(document).ready(function() {
-	$('.js-example-basic-single').select2({
-		theme: "my",
-	});
-	$('.js-example-basic-single2').select2({
-		theme: "my-one-line",
-	});
-});
